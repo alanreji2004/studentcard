0e6dab1f54bc78a9d0f36fa0c93521e3cb482d91
alanreji2004 <alenreji2004@gmail.com>
2025-04-11 00:28:05 +0530

    made file structure

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import styles from "./Navbar.module.css"
+import { Menu } from "lucide-react"
+import logo from "./logo.svg"
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false)
+  const navigate = useNavigate()
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("isLoggedIn")
+    navigate("/login")
+  }
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logoContainer}>
+        <img src={logo} alt="Logo" className={styles.logo} />
+        <div className={styles.titleContainer}>
+          <span className={styles.title}>COLLEGE OF ENGINEERING PERUMON PTA</span>
+          <span className={styles.subtitle}>Under the Cooperative Academy of Professional Education (CAPE)</span>
+          <span className={styles.established}>Established by <span className={styles.govt}>Govt. of Kerala</span></span>
+        </div>
+      </div>
+      <div className={styles.menuContainer}>
+        <button className={styles.menuButton} onClick={toggleMenu} aria-label="Toggle Menu">
+          <Menu size={24} />
+        </button>
+        <ul className={`${styles.menu} ${isOpen ? styles.showMenu : ""}`} style={{ marginTop: isOpen ? '10px' : '0' }}>
+          <li><Link onClick={handleLogout} className={styles.menuItem}>Logout</Link></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import * as React from "react"
+import { createRoot } from "react-dom/client"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+import Login from "./pages/Login/Login"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+])
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 )
